3192c7d3bda5d61d2f136c8cf8efd6422fdd7a6d
ThinkShout Automation <dev-team+pantheon@thinkshout.com>
2019-10-13 16:25:30 -0700

    Adding the fresh install option.

--- RoboFile.php
+++ RoboFile.php
@@ -4,10 +4,17 @@
  *
  * @see http://robo.li/
  */
-
-
 class RoboFile extends \ThinkShout\RoboDrupal\Tasks
 {
-
-
+  /**
+   * Determines the database to start from when doing new work on this project.
+   *
+   * @return mixed
+   *   Return the Pantheon environment you want to pull in on install (live,
+   *   dev, etc), or FALSE to install from scratch.
+   */
+  protected function databaseSourceOfTruth() {
+    // Until we have a working stable DB on Pantheon, leave this as "FALSE".
+    return FALSE;
+  }
 }
